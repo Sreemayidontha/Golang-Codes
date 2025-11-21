58815285cf03838701bc3cfd2b4e2b6fd1626905
Sreemayidontha <sreemayi.dontha@gmail.com>
2025-11-20 23:19:35 -0500

    Update files

---
+++ anonymous_fun.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func GFG(a string) string {
+	return "Geeks"
+
+}
+func main() {
+	func() {
+		fmt.Println("Anonymous Function Called")
+	}()
+	value := func(msg string) {
+		fmt.Println("Hello " + msg)
+	}
+	value("Sreemayi")
+	message := func(p, q string) string {
+		return p + q + GFG("Geeks")
+	}
+	func() {
+		fmt.Println(message("Geeks", "For"))
+	}()
+
+}

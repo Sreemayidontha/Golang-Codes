6fe719df6eaf24f775fecf17ba317f38882c5e4f
Sreemayidontha <sreemayi.dontha@gmail.com>
2025-12-11 23:59:42 -0500

    ..

---
+++ capacity_pointer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [8]int{200, 300, 400,
+		500, 600, 700, 100, 200}
+	fmt.Println("Capacity of arr: ", cap(arr))
+
+	var b [5]*int
+	fmt.Println("Capacity of b: ", cap(b))
+	var x int
+	for x := 0; x < len(b); x++ {
+		b[x] = &arr[x]
+	}
+	for x = 0; x < len(b); x++ {
+		fmt.Printf("Value of b[%d] =%d", *b[x])
+	}
+	fmt.Println("Capacity of b :", cap(b))
+
+}
